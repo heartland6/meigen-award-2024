fe99e822242eaa798aceae17b46d774d837ba8a4
Riku Nakajima <riku_naka_jima@icloud.com>
2024-12-29 00:12:15 +0900

    GAS側でAPIをうまく作れたので簡略化、formの送信先をGASに

---
+++ gas.js
@@ -0,0 +1,25 @@
+const meigenGASKey = '114-zasshibu-ikisugi-514';
+const meigenGASId = 'AKfycbxzxep1WU5rRIOgJ0kBt9aPz4ziXAie4oM5c2fUk3_8UtVRAbM3zi6Lmp_KPpZ0ZyIx';
+const rankGASId = "AKfycbytIe4kinJGQKg8wm8exHELxS-D4Vy5y_a2dHfTGnMVEyeBYaZLXQPyZzjf48t4oRVA";
+
+const gasGet = async (gasId, params) => {
+    const gasUrl = `https://script.google.com/macros/s/${gasId}/exec` + `?key=${meigenGASKey}`;
+    const paramString = Object.entries(params).map(param => {
+        console.log(param);
+        return `&${param[0]}=${JSON.stringify(param[1])}`;
+    }).join('');
+    console.log(paramString);
+
+    return await fetch(gasUrl + paramString)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        return data;
+    });
+}
+
+const getMeigenInfo = async meigenId => {
+    const info = await gasGet(rankGASId, {id: meigenId});
+    console.log(info);
+    return info;
+}
