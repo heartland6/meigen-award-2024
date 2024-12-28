f76d7eb3bb1e576fcdbfa3c061209ab27447a3a8
Riku Nakajima <riku_naka_jima@icloud.com>
2024-12-28 09:03:27 +0900

    各種マージン調整、票数のinputをきちんと隠した

--- meigen.js
+++ meigen.js
@@ -1,20 +1,19 @@
 let isMeigenLoading = false;
 
 const getMeigen = async meigenId => {
-    isMeigenLoading = true;
-
     const meigenGASId = 'AKfycbxzxep1WU5rRIOgJ0kBt9aPz4ziXAie4oM5c2fUk3_8UtVRAbM3zi6Lmp_KPpZ0ZyIx';
     const meigenGASKey = '114-zasshibu-ikisugi-514';
     const meigenGetterUrl = `https://script.google.com/macros/s/${meigenGASId}/exec` + `?key=${meigenGASKey}`;
 
-    const meigenString = await fetch(meigenGetterUrl + `&id=${JSON.stringify([meigenId])}`)
+    const meigen = await fetch(meigenGetterUrl + `&id=${JSON.stringify([meigenId])}`)
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
-        return data.results[0];
+        return data;
     });
-    console.log(meigenString);
+    console.log(meigen);
 
+    const meigenString = meigen.results[0];
     const meigenContentRegex = meigenString.match(/“.+”/s);
     const meigenAuthorRegex = meigenString.match(/──.+/);
     console.log(meigenContentRegex);
@@ -23,7 +22,9 @@ const getMeigen = async meigenId => {
     const meigenContent = meigenContentRegex[0].replace(/[“”]/g,'');
     const meigenAuthor = meigenAuthorRegex[0];
 
-    isMeigenLoading = false;
-
-    return {content: meigenContent, author: meigenAuthor};
+    return {
+        content: meigenContent,
+        author: meigenAuthor,
+        isRandom: meigen.isRandom,
+    }
 }
