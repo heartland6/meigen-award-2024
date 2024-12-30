4b50b7e9377e4189f229eb00505359839eeaebdb
Riku Nakajima <riku_naka_jima@icloud.com>
2024-12-31 01:31:06 +0900

    新ボタン3つを追加&+N票ボタンの常時表示

--- showMeigen.js
+++ showMeigen.js
@@ -9,16 +9,27 @@ const showMeigen = async () => {
         await sleep(2000);
     }
 
-    const meigen = await getMeigenInfo(Number(inputMeigenId));
+    const meigenId = Number(inputMeigenId);
+    const meigen = await getMeigenInfo(meigenId);
     if (meigen.isOutOfRange) {
         document.getElementById('loadMeigen').innerHTML = '見つかりません';
         await sleep(2000);
     } else {
-        document.getElementById('current-votes').innerHTML = `$meigen${inputMeigenId}の得票数:`;
+        document.getElementById('current-votes').innerHTML = `$meigen${meigenId}の得票数:`;
         document.getElementById('meigenContent').innerHTML = `${meigen.content}`;
-        document.getElementById('meigenAuthor').innerHTML = `──${meigen.author} | 名言#${inputMeigenId}`;
+        document.getElementById('meigenAuthor').innerHTML = `──${meigen.author} | 名言#${meigenId}`;
         document.getElementById('vote-count').innerHTML = meigen.votes;
     }
 
     document.getElementById('loadMeigen').innerHTML = '$meigenを表示';
+    
+    // ねとらぼ
+    const netoraboMeigenIds = [345,346];
+    if (netoraboMeigenIds.includes(meigenId)) showPlusButton(meigenId);
+    // 栗田穣崇
+    const kuritaMeigenIds = [280,281,339];
+    if (kuritaMeigenIds.includes(meigenId)) showMinusButton(meigenId);
+    // バオバブ系
+    const baobabMeigenIds = [106,128,243,321,372,506,532];
+    if (baobabMeigenIds.includes(meigenId)) showRandomButton(meigenId);
 }
--- voteFeedback.js
+++ voteFeedback.js
@@ -1,67 +1,119 @@
 // ボタンごとのフィードバック
 
 // 「+1票」アニメーションスクリプト by chatGPT
-const popVoteFeedback = (votes, feedbackContainerName) => {
+const popClickFeedback = (text,color) => {
     if (document.getElementById('meigenId').value === '') return null;
 
-    const container = document.getElementById(feedbackContainerName);
-
+    const container = document.getElementById('vote-feedback-container');
     // 新しい「+1票」要素を作成
     const feedback = document.createElement('span');
-    feedback.textContent = `+${votes}票`;
+    feedback.textContent = text;
     feedback.className = 'vote-feedback';
-
+    feedback.style.color = color;
     // コンテナに追加
     container.appendChild(feedback);
-
     // アニメーション終了後に要素を削除
     feedback.addEventListener('animationend', () => {
         feedback.remove();
     });
 }
 
-const changeVotesNumber = newVotes => {
+const getRandomVotesNumber = () => {
+    const sign = Math.random() < 0.7 ? +1 : -1;
+    const rand2 = (Math.random() * Math.random()) ** 2;
+    const result = sign * Math.trunc((rand2 * 1e5) + 10);
+    return result;
+}
+const orgRound = (value, base) => Math.round(value * base) / base;
+
+const addVotesNumber = newVotes => {
+    if (document.getElementById('meigenId').value === '') return null;
+
+    document.getElementById('votes').setAttribute('value',newVotes);
+
     const voteCount = document.getElementById('vote-count');
     const currentVotes = Number(voteCount.getHTML());
     voteCount.innerHTML = currentVotes + newVotes;
 }
 
+const red = '#ff5733';
+const blue = '#3357ff';
+
+// 通常の投票ボタン(+N票ボタン)
 document.getElementById('one-vote-button').addEventListener('click', () => {
-    document.getElementById('votes').setAttribute('value',1);
-    popVoteFeedback(1, 'one-vote-feedback-container');
-    changeVotesNumber(1);
+    popClickFeedback('+1票',red);
+    addVotesNumber(1);
 });
 document.getElementById('ten-vote-button').addEventListener('click', () => {
-    document.getElementById('votes').setAttribute('value',10);
-    popVoteFeedback(10, 'ten-vote-feedback-container');
-    changeVotesNumber(10);
+    popClickFeedback('+10票',red);
+    addVotesNumber(10);
 });
 document.getElementById('hundred-vote-button').addEventListener('click', () => {
-    document.getElementById('votes').setAttribute('value',100);
-    popVoteFeedback(100, 'hundred-vote-feedback-container');
-    changeVotesNumber(100);
+    popClickFeedback('+100票',red);
+    addVotesNumber(100);
 });
 
+// 変な投票ボタン
+const plusButton = document.getElementById('plus-vote-button');
+const minusButton = document.getElementById('minus-vote-button');
+const randomButton = document.getElementById('random-vote-button');
 
-// ボタン全体のクリック数に基づくフィードバック
-const tenVoteButton = document.getElementById('ten-vote');
-const hundredVoteButton = document.getElementById('hundred-vote');
-tenVoteButton.style.display = 'none';
-hundredVoteButton.style.display = 'none';
+const advancedButtonsStatus = {
+    plusButton: false,
+    minusButton: false,
+    randomButton: false,
+}
 
-const state = {totalClicks: 0};
-const voteButtons = document.querySelectorAll('.vote-button');
-voteButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (document.getElementById('meigenId').value === '') return null;
-        
-        state.totalClicks++;
+const showPlusButton = meigenId => {
+    advancedButtonsStatus.plusButton = true;
+    document.getElementById("plus-vote-button").innerHTML = '水増し (数%増加)';
 
-        if (state.totalClicks >= 10) {
-            tenVoteButton.style.display = 'block';
-        }
-        if (state.totalClicks >= 30) {
-            hundredVoteButton.style.display = 'block';
-        }
-    });
+    const message = `$meigen${meigenId} を見つけてくれてありがとナス！ `+ '\n' +'【「水増し」ボタンが解放されました】';
+    alert(message);
+}
+const showMinusButton = meigenId => {
+    advancedButtonsStatus.minusButton = true;
+    document.getElementById("minus-vote-button").innerHTML = 'ネガキャン (数%減少)';
+
+    const message = `$meigen${meigenId} を見つけてくれてありがとナス！ `+ '\n' +'【「ネガキャン」ボタンが解放されました】';
+    alert(message);
+}
+const showRandomButton = meigenId => {
+    advancedButtonsStatus.randomButton = true;
+    document.getElementById("random-vote-button").innerHTML = 'ランダム (-10万〜+10万票)';
+
+    const message = `$meigen${meigenId} を見つけてくれてありがとナス！` + '\n' +'【「ランダム」ボタンが解放されました】';
+    alert(message);
+}
+
+plusButton.addEventListener('click', () => {
+    if (!(advancedButtonsStatus.plusButton)) return;
+
+    const currentVotes = Number(document.getElementById('vote-count').getHTML());
+    if (currentVotes === 0) return;
+
+    const newVotesRate = orgRound(1 + Math.random() * 4, 100);
+    popClickFeedback(`${newVotesRate}%増加`,red);
+    const effVotes = Math.trunc(Math.abs(currentVotes) * (newVotesRate / 100));
+    addVotesNumber(effVotes);
+});
+minusButton.addEventListener('click', () => {
+    if (!(advancedButtonsStatus.minusButton)) return;
+
+    const currentVotes = Number(document.getElementById('vote-count').getHTML());
+    if (currentVotes === 0) return;
+
+    const newVotesRate = orgRound(1 + Math.random() * 4, 100);
+    popClickFeedback(`${newVotesRate}%減少`,blue);
+    const effVotes = -1 * Math.trunc(Math.abs(currentVotes) * (newVotesRate / 100));
+    addVotesNumber(effVotes);
+});
+randomButton.addEventListener('click', () => {
+    if (!(advancedButtonsStatus.randomButton)) return;
+
+    const votes = getRandomVotesNumber();
+    const votesText = votes > 0 ? `+${votes}票` : `${votes}票`;
+    const votesTextColor = votes > 0 ? red : blue;
+    popClickFeedback(votesText,votesTextColor);
+    addVotesNumber(votes);
 });
